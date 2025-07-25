5f5fc0a21f37bb18a2aa2e8ca73cc4699bf31ef1
Akai-Hikari <silentyamato10@gmail.com>
2025-07-25 20:46:23 +0700

    menyelesaikan tugas praktikum 1: route dengan Laravel

--- Belajar01_Laravel/routes/web.php
+++ Belajar01_Laravel/routes/web.php
@@ -3,6 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "<h1>Hi! Selamat Datang di Website Laravel</h1>";
+});
+
+Route::get('/about',function () {
+    echo "<h2> Nama: Aldebaran sabian fatihah </h2>";
+    echo "<h2> Kelas: 11 RPB </h2>";
+    echo "<h2> NIS: 24988 </h2>";
+});
+
+Route::get('/article/{id}',function ($id) {
+    switch($id){
+    case 1:
+        echo "ini adalah artikel halaman pertama \n";
+        break;
+    case 2:
+        echo "ini adalah artikel halaman kedua \n";
+        break;
+    default:
+        echo "halaman tidak di temukan!, silahkan cari lagi~ \n";
+        break;
+    }
 });
 
