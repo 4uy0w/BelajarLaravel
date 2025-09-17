a83cc9abf10cf21a2060fb7fc18f8401f595ffaf
Hikari <silentyamato10@gmail.com>
2025-09-17 16:27:58 +0700

    menambahkan ShrineComcis untuk tugas CRUD Laravel

--- siakad/resources/views/layout.blade.php
+++ siakad/resources/views/layout.blade.php
@@ -3,8 +3,8 @@
 <html>
     <head>
         <title> Sistem Manajemen Akademik Siswa </title>
-        <link rel="stylesheet" href="http://HikariServer.id/resource/style/style-CRUD-sekolah.css">
-        <script src="http://HikariServer.id/resource/script/script-CRUD-sekolah.js"></script>
+        <link rel="stylesheet" href="http://localhost/resource/style/style-CRUD-sekolah.css">
+        <script src="http://localhost/resource/script/script-CRUD-sekolah.js"></script>
     </head>
     <body>
         <div class="container">
